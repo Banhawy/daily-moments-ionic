fa053c9d19076641577697ed23afe471bc949f82
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-15 22:16:36 +0200

    Swapped in IonReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import {
-  IonApp
+  IonApp, IonRouterOutlet
 } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
 import React from 'react';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
 
 const App: React.FC = () => {
   return (
     <IonApp>
-      <BrowserRouter>
-        <Route exact path="/home">
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
             <HomePage />
-        </Route>
-        <Route exact path="/settings">
+          </Route>
+          <Route exact path="/settings">
             <SettingsPage />
-        </Route>
-        <Redirect exact path="/" to="/home" />
-      </BrowserRouter>
+          </Route>
+          <Redirect exact path="/" to="/home" />
+        </IonRouterOutlet>
+      </IonReactRouter>
     </IonApp>
   );
 };
