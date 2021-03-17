5d19cbd74c26aecdf73d5514ab95a60932ce6e4b
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-17 19:30:53 +0200

    fetched data and displayed in ion list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import {
   IonContent,
   IonHeader,
+  IonItem,
+  IonList,
   IonPage,
-  IonRouterLink,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
 import React from 'react';
+import { entries } from '../data';
+
 
 const HomePage: React.FC = () => {
   return (
@@ -17,7 +20,14 @@ const HomePage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        This is the home page...
+        <IonList>
+          {entries.map(entry =>
+            <IonItem button key={entry.id}
+              routerLink={`/entries/${entry.id}`}>
+              {entry.title}
+            </IonItem>
+          )}
+        </IonList>
       </IonContent>
     </IonPage>
   );
