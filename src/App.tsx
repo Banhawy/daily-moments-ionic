47ec6b55e50e22c488f5449d0926204327285cc3
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-18 01:34:26 +0200

    added login page and route

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,28 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { home as homeIcon, settings as settingsIcon } from "ionicons/icons";
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
 import EntryPage from './pages/EntryPage';
+import LoginPage from './pages/LoginPage';
 
 const App: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+  console.log(`logged in status: ${loggedIn}`)
   return (
     <IonApp>
       <IonReactRouter>
         <IonTabs>
           <IonRouterOutlet>
+            <Route exact path="/login">
+              {!loggedIn ? 
+                <LoginPage onLogin={() => setLoggedIn(true)}/> :
+                <Redirect to="/entries" />}
+            </Route>
             <Route exact path="/entries">
-              <HomePage />
+              {loggedIn ? <HomePage /> : <Redirect to="/login" />}
             </Route>
             <Route exact path="/settings">
               <SettingsPage />
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,30 @@
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React from 'react';
+
+interface LoggedInProps {
+  onLogin: () => void
+}
+
+const LoginPage: React.FC<LoggedInProps> = ({ onLogin }) => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonButton expand="block" onClick={onLogin}>Login</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default LoginPage;
