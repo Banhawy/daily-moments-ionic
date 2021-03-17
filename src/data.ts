717dfc87149204849b4cebfa6933e96862af2900
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-17 19:30:35 +0200

    added dummy data to use with list

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const entries = [
+    {
+        id: '1',
+        title: 'First Entry',
+        description: 'This is the first title'
+    },
+    {
+        id: '2',
+        title: 'Second Entry',
+        description: 'This is the Second title'
+    },
+    {
+        id: '3',
+        title: 'Third Entry',
+        description: 'This is the Third title'
+    }
+]
