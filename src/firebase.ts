e92fbcd21aed7759cd0bdd199645d47c9c7b071a
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-24 21:50:45 +0200

    added firbase config

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firbase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvj-vtm6HXKnHcOlXMLDN2nMyiXv1hR2w",
+    authDomain: "ionic-daily-moments.firebaseapp.com",
+    projectId: "ionic-daily-moments",
+    storageBucket: "ionic-daily-moments.appspot.com",
+    messagingSenderId: "402359474975",
+    appId: "1:402359474975:web:a2cc5246fbab3caf8c2e2a"
+  };
+
+const app = firbase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
