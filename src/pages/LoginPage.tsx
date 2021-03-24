a359b42b8dfaee9d6c6e3b4cec4c2ad6ed91fb8a
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-24 22:03:54 +0200

    added login form and auth with firebase

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,13 +2,19 @@ import {
   IonButton,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router';
 import { useAuth } from '../auth';
+import { auth } from '../firebase';
+
 
 interface LoggedInProps {
   onLogin: () => void;
@@ -16,6 +22,17 @@ interface LoggedInProps {
 
 const LoginPage: React.FC<LoggedInProps> = ({ onLogin }) => {
   const { loggedIn } = useAuth()
+  const [email, setEmail] = useState<string | null | undefined>('');
+  const [password, setPassword] = useState<string | null | undefined>('');
+  
+  const handleLogin = async () => {
+    if (email && password) {
+      const credentials = await auth.signInWithEmailAndPassword(email, password)
+      console.log('credentials: ', credentials)
+      onLogin();
+    }
+  }
+
   if (loggedIn) {
     return <Redirect to="/my/entries" />
   }
@@ -27,7 +44,21 @@ const LoginPage: React.FC<LoggedInProps> = ({ onLogin }) => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonButton expand="block" onClick={onLogin}>Login</IonButton>
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Email</IonLabel>
+            <IonInput type="email" value={email}
+              onIonChange={event => setEmail(event.detail.value)}
+             />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Password</IonLabel>
+            <IonInput type="password" value={password}
+              onIonChange={event => setPassword(event.detail.value)}
+             />
+          </IonItem>
+        </IonList>
+        <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
       </IonContent>
     </IonPage>
   );
